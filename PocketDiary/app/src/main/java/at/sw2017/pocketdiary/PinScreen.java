358490ba9da614f339235c13f885615f7b2ceba6
Nicole Schuster <n.schuster@student.tugraz.at>
2017-05-04 15:03:47 +0200

    [NS, AR] design for pin screen

---
+++ PocketDiary/app/src/main/java/at/sw2017/pocketdiary/PinScreen.java
@@ -0,0 +1,16 @@
+package at.sw2017.pocketdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Window;
+import android.widget.EditText;
+
+public class PinScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_screen);
+    }
+}
