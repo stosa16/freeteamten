43e469677d7d5336623891fea83a1f47b8461442
Anja Reibenbacher <anja.reibenbacher@student.tugraz.at>
2017-05-05 14:06:55 +0200

    [AR, NS] switch on/off listener

--- PocketDiary/app/src/main/java/at/sw2017/pocketdiary/SettingScreen.java
+++ PocketDiary/app/src/main/java/at/sw2017/pocketdiary/SettingScreen.java
@@ -2,6 +2,9 @@ package at.sw2017.pocketdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class SettingScreen extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class SettingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
+
+        //Switch on/off
+        final TextView tView = (TextView) findViewById(R.id.on_off);
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on) {
+                    //todo database insert
+                    tView.setText("ON");
+                } else {
+                    //todo database insert
+                    tView.setText("OFF");
+                }
+            }
+        });
+
     }
+
 }
