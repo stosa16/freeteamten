7eb98cbbd4fa6a5ecea6fa297ea9b1a51f0b6e4f
Sandra Stocker <sandra.stocker@student.tugraz.at>
2017-05-06 11:22:06 +0200

    [KS, SS] Design Create Entry Screen

---
+++ PocketDiary/app/src/main/java/at/sw2017/pocketdiary/CreateEntryScreen.java
@@ -0,0 +1,12 @@
+package at.sw2017.pocketdiary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateEntryScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_entry_screen);
+    }
+}
