41b2abadf02c699db8555a159dc193ce009e3053
Nicole Schuster <n.schuster@student.tugraz.at>
2017-05-22 15:43:27 +0200

    [NS, AR] profile picture icon

--- PocketDiary/app/src/main/java/at/sw2017/pocketdiary/StartScreen.java
+++ PocketDiary/app/src/main/java/at/sw2017/pocketdiary/StartScreen.java
@@ -1,9 +1,13 @@
 package at.sw2017.pocketdiary;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -19,6 +23,7 @@ public class StartScreen extends AppCompatActivity {
         final Button create_entry_button =(Button) findViewById(R.id.create_entry);
         final Button review_button =(Button) findViewById(R.id.review);
         final Button statistic_button =(Button) findViewById(R.id.statistic);
+        final ImageButton picture_button = (ImageButton) findViewById(R.id.picture);
 
         setting_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -59,5 +64,40 @@ public class StartScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        picture_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                picture_button.setBackgroundColor(Color.WHITE);
+
+                //loadingPopup(picture_button);
+                //Intent intent = new Intent(StartScreen.this, SettingScreen.class);
+                //startActivity(intent);
+            }
+        });
     }
+
+    /*public void loadingPopup(ImageButton picture_button) {
+
+        PopupMenu popUpMenu = new PopupMenu(getApplicationContext(), picture_button);
+        popUpMenu.inflate(R.menu.popup);
+        popUpMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_camera:
+                        break;
+
+                    case R.id.action_gallery:
+                        break;
+
+                    default:
+                        break;
+
+                }
+                return true;
+            }
+        });
+        popUpMenu.show();
+
+    }*/
 }
