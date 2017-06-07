00cd48aaf1f53e780744fc04e1899df3fa05dd34
Anja Reibenbacher <anja.reibenbacher@student.tugraz.at>
2017-06-07 16:31:05 +0200

    [AR, AM] fix tests

--- PocketDiary/app/src/androidTest/java/at/sw2017/pocketdiary/SettingScreenInstrumentedTest.java
+++ PocketDiary/app/src/androidTest/java/at/sw2017/pocketdiary/SettingScreenInstrumentedTest.java
@@ -1,6 +1,10 @@
 package at.sw2017.pocketdiary;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -15,6 +19,7 @@ import org.junit.runner.RunWith;
 import java.util.List;
 
 import at.sw2017.pocketdiary.business_objects.UserSetting;
+import at.sw2017.pocketdiary.database_access.DBHandler;
 import at.sw2017.pocketdiary.database_access.DBUserSetting;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -28,8 +33,11 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
 
@@ -41,7 +49,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class SettingScreenInstrumentedTest {
 
-    private String titleToBeTyped;
+    private DBHandler dbh;
 
     @Rule
     public ActivityTestRule<SettingScreen> mActivityRule = new ActivityTestRule<SettingScreen>(SettingScreen.class);
@@ -56,9 +64,22 @@ public class SettingScreenInstrumentedTest {
 
     @Test
     public void testSwitch() throws Exception {
-        onView(withId(R.id.switch1)).check(matches(not(isChecked())));
-        onView(withId(R.id.switch1)).perform(click());
-        onView(withId(R.id.switch1)).check(matches(isChecked()));
+
+        //boolean checked = mActivityRule.getActivity().findViewById(R.id.switch1).isChecked();
+        //boolean checked = onView(withId(R.id.checkbox)).check(matches(not(isChecked())));
+
+        //onView(withId(R.id.switch1)).perform(setChecked.setChecked(true));
+        /*if(checked){
+            onView(withId(R.id.switch1)).check(matches(isChecked()));
+            onView(withId(R.id.switch1)).perform(click());
+            onView(withId(R.id.switch1)).check(matches(not(isChecked())));
+        }
+        else {*/
+            onView(withId(R.id.switch1)).check(matches(not(isChecked())));
+            onView(withId(R.id.switch1)).perform(click());
+            onView(withId(R.id.switch1)).check(matches(isChecked()));
+        //}
+
     }
 
     @Test
@@ -88,12 +109,12 @@ public class SettingScreenInstrumentedTest {
         checkInput.check_user_name(name, userSetting, user_name);
         assertEquals("user1", userSetting.getUserName());
 
-        userSetting.setPin("a");
+        userSetting.setPin("aaaaa");
         String saved_pin = userSetting.getPin();
 
-        onView(withId(R.id.current_pin)).perform(typeText("b"), closeSoftKeyboard());
-        onView(withId(R.id.new_pin1)).perform(typeText("def"), closeSoftKeyboard());
-        onView(withId(R.id.new_pin2)).perform(typeText("def"), closeSoftKeyboard());
+        onView(withId(R.id.current_pin)).perform(typeText("bbbbb"), closeSoftKeyboard());
+        onView(withId(R.id.new_pin1)).perform(typeText("defgh"), closeSoftKeyboard());
+        onView(withId(R.id.new_pin2)).perform(typeText("defgh"), closeSoftKeyboard());
 
         EditText old_pin = (EditText) mActivityRule.getActivity().findViewById(R.id.current_pin);
         EditText new_ = (EditText) mActivityRule.getActivity().findViewById(R.id.new_pin1);
@@ -109,14 +130,14 @@ public class SettingScreenInstrumentedTest {
         ret = checkInput.check_old_pin(old_pin.getText().toString(), saved_pin, new_.getText().toString(), repeat_.getText().toString());
         assertEquals(ret, false);
 
-        onView(withId(R.id.current_pin)).perform(typeText("a"), closeSoftKeyboard());
+        onView(withId(R.id.current_pin)).perform(typeText("aaaaa"), closeSoftKeyboard());
         old_pin = (EditText) mActivityRule.getActivity().findViewById(R.id.current_pin);
 
         ret = checkInput.check_old_pin(old_pin.getText().toString(), saved_pin, new_.getText().toString(), repeat_.getText().toString());
         assertEquals(ret, true);
 
         onView(withId(R.id.new_pin2)).perform(clearText());
-        onView(withId(R.id.new_pin2)).perform(typeText("de"), closeSoftKeyboard());
+        onView(withId(R.id.new_pin2)).perform(typeText("defghi"), closeSoftKeyboard());
         new_ = (EditText) mActivityRule.getActivity().findViewById(R.id.new_pin1);
         repeat_ = (EditText) mActivityRule.getActivity().findViewById(R.id.new_pin2);
 
@@ -124,23 +145,27 @@ public class SettingScreenInstrumentedTest {
         assertEquals(ret, false);
 
         onView(withId(R.id.new_pin2)).perform(clearText());
-        onView(withId(R.id.new_pin2)).perform(typeText("def"), closeSoftKeyboard());
+        onView(withId(R.id.new_pin2)).perform(typeText("defgh"), closeSoftKeyboard());
         new_ = (EditText) mActivityRule.getActivity().findViewById(R.id.new_pin1);
         repeat_ = (EditText) mActivityRule.getActivity().findViewById(R.id.new_pin2);
         ret = checkInput.check_new_pin(new_.getText().toString(), repeat_.getText().toString(), userSetting);
         assertEquals(ret, true);
-        assertEquals("def", userSetting.getPin());
+        assertEquals("defgh", userSetting.getPin());
     }
 
     @Before
     public void setUp() {
         Intents.init();
         mActivityRule.launchActivity(new Intent());
-        titleToBeTyped = "Running";
+        Context context = InstrumentationRegistry.getTargetContext();
+        context.deleteDatabase(DBHandler.DATABASE_NAME);
+        dbh = new DBHandler(context);
+        TestHelper.createUserSettingEmpty(context);
     }
 
     @After
     public void release() {
+        dbh.close();
         Intents.release();
     }
 
@@ -149,11 +174,11 @@ public class SettingScreenInstrumentedTest {
         DBUserSetting dbUserSetting = new DBUserSetting(mActivityRule.getActivity());
         List<UserSetting> temp = dbUserSetting.getUserSetting(1);
         UserSetting userSetting = temp.get(0);
-        userSetting.setPin("a");
+        userSetting.setPin("aaaaa");
         dbUserSetting.update(userSetting, 1);
-        onView(withId(R.id.current_pin)).perform(typeText("a"), closeSoftKeyboard());
-        onView(withId(R.id.new_pin1)).perform(typeText("b"), closeSoftKeyboard());
-        onView(withId(R.id.new_pin2)).perform(typeText("b"), closeSoftKeyboard());
+        onView(withId(R.id.current_pin)).perform(typeText("aaaaa"), closeSoftKeyboard());
+        onView(withId(R.id.new_pin1)).perform(typeText("bbbbb"), closeSoftKeyboard());
+        onView(withId(R.id.new_pin2)).perform(typeText("bbbbb"), closeSoftKeyboard());
         onView(withId(R.id.save)).perform(click());
         intended(hasComponent(StartScreen.class.getName()));
     }
