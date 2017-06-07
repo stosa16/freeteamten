0e4113680254bed7989a847e4436c347015d81a2
Sandra Stocker <sandra.stocker@student.tugraz.at>
2017-06-07 19:08:43 +0200

    [SS, KS] Seperate Location tests due to different testing conditions necessary

---
+++ PocketDiary/app/src/androidTest/java/at/sw2017/pocketdiary/CreateEntryLocationInstrumentedTest.java
@@ -0,0 +1,166 @@
+package at.sw2017.pocketdiary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.DatePicker;
+
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import at.sw2017.pocketdiary.database_access.DBAddress;
+import at.sw2017.pocketdiary.database_access.DBEntry;
+import at.sw2017.pocketdiary.database_access.DBHandler;
+import at.sw2017.pocketdiary.database_access.DBPicture;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * Created by Sandra on 07.06.2017.
+ */
+
+public class CreateEntryLocationInstrumentedTest {
+
+    private DBHandler dbh;
+    private DBEntry dbe;
+    private DBAddress dba;
+    private DBPicture dbp;
+    private Geocoder geocoder;
+    private Context context;
+
+    private String titleToBeTyped = "Running";
+
+    @Rule
+    public ActivityTestRule<CreateEntryScreen> mActivityRule = new ActivityTestRule<>(CreateEntryScreen.class, false, false);
+
+    @Before
+    public void setUp() {
+        context = getTargetContext();
+        context.deleteDatabase(DBHandler.DATABASE_NAME);
+        dbh = new DBHandler(context);
+        Helper.initCategories(context);
+        dbe = new DBEntry(context);
+        dba = new DBAddress(context);
+        dbp = new DBPicture(context);
+        geocoder = new Geocoder(context);
+        Intents.init();
+        mActivityRule.launchActivity(new Intent());
+    }
+
+    @After
+    public void release() {
+        dbh.close();
+        dbe.close();
+        dba.close();
+        dbp.close();
+        Intents.release();
+    }
+
+    @Test
+    public void pressLocationButton() {
+        TestHelper.grantLocationPermissions();
+        onView(withId(R.id.input_title)).perform(typeText(titleToBeTyped), closeSoftKeyboard());
+        onView(withId(R.id.input_category)).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(1).perform(click());
+        onView(withId(R.id.input_category)).check(matches(not(withText("Sport"))));
+        onView(withId(R.id.input_subcategory)).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(2).perform(click());
+        onView(withId(R.id.input_subcategory)).check(matches(not(withText("Running"))));
+        onView(withId(R.id.btn_calendar)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 4, 3));
+        onView(withId(android.R.id.button1)).perform(click()); //click on dialog positive button
+        onView(withId(R.id.btn_location)).perform(click());
+        onView(withId(R.id.badge_address)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkGetLocationWithPermission() {
+        TestHelper.grantLocationPermissions();
+        GpsLocation gps_location = new GpsLocation(context);
+        Location current_location = new Location("A1");
+        current_location.setLongitude(15);
+        current_location.setLatitude(13);
+        gps_location.current_location = current_location;
+
+        Location location = gps_location.getLocation();
+        assertTrue(location.getLatitude() != 0);
+    }
+
+    /* ======================== Files with disabled GPS on Phone ============================== */
+    /*@Test
+    public void shouldOpenAlertDialog(){
+        onView(withId(R.id.btn_location)).perform(click());
+        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldCancelAlertDialog(){
+        onView(withId(R.id.btn_location)).perform(click());
+        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
+        onView(withText("No")).perform(click());
+        onView(withId(R.id.btn_location)).perform(click());
+        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldAcceptAlertDialog() throws Throwable {
+        onView(withId(R.id.btn_location)).perform(click());
+        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
+        onView(withText("Yes")).perform(click());
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mActivityRule.getActivity().onBackPressed();
+            }
+        });
+        intended(hasComponent(CreateEntryScreen.class.getName()));
+    }
+
+    @Test
+    public void checkGetLocation() {
+        TestHelper.grantLocationPermissions();
+        GpsLocation gps_location = new GpsLocation(context);
+        //enable GPS to test this
+        Location current_location = new Location("A1");
+        current_location.setLongitude(15);
+        current_location.setLatitude(13);
+        gps_location.current_location = current_location;
+        assertTrue(gps_location.getLocation() != null);
+        //disable GPS to test this
+        //gps_location.current_location = null;
+        //assertTrue(gps_location.getLocation() == null);
+    }*/
+
+    /* Set Lat/Long of Emulator to 13.0/43.0*/
+    /*@Test
+    public void reverseGeocoding() throws IOException {
+        Address address = new Address(13.0, 43.0);
+        ReverseGeocoder reverseGeocoder = new ReverseGeocoder();
+        Address address1;
+        address1 = reverseGeocoder.getAddress(address.getLongitude(), address.getLatitude(), geocoder);
+        SystemClock.sleep(2000);
+        assertTrue(address1.getCountry().equals("Italy"));
+    }*/
+}
--- PocketDiary/app/src/androidTest/java/at/sw2017/pocketdiary/CreateEntryScreenInstrumentedTest.java
+++ PocketDiary/app/src/androidTest/java/at/sw2017/pocketdiary/CreateEntryScreenInstrumentedTest.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Geocoder;
-import android.location.Location;
 import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.contrib.PickerActions;
@@ -291,30 +290,6 @@ public class CreateEntryScreenInstrumentedTest {
     }
 
     @Test
-    public void createEntryWithAddressStreet() {
-        Address address_test = new Address(23, 45);
-        address_test.setStreet("Inffeldgasse 10");
-        onView(withId(R.id.input_title)).perform(typeText(titleToBeTyped), closeSoftKeyboard());
-        onView(withId(R.id.input_category)).perform(click());
-        onData(allOf(is(instanceOf(String.class)))).atPosition(1).perform(click());
-        onView(withId(R.id.input_category)).check(matches(not(withText("Sport"))));
-        onView(withId(R.id.input_subcategory)).perform(click());
-        onData(allOf(is(instanceOf(String.class)))).atPosition(2).perform(click());
-        onView(withId(R.id.input_subcategory)).check(matches(not(withText("Running"))));
-        onView(withId(R.id.btn_calendar)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 4, 3));
-        onView(withId(android.R.id.button1)).perform(click()); //click on dialog positive button
-        mActivityRule.getActivity().entry_address = address_test;
-        onView(withId(R.id.btn_save)).perform(click());
-        Entry entry;
-        Context context = getTargetContext();
-        entry = Helper.getEntryComplete(context, 1);
-        DecimalFormat df2 = new DecimalFormat("###.##");
-        double latitude = Double.valueOf(df2.format(entry.getAddress().getLatitude()));
-        assertTrue(address_test.getLatitude() == latitude);
-    }
-
-    @Test
     public void createEntryWithPictures() {
         TestHelper.grantPicturePermissions();
         String path_one = "/test/1.jpg";
@@ -346,6 +321,30 @@ public class CreateEntryScreenInstrumentedTest {
     }
 
     @Test
+    public void createEntryWithAddressStreet() {
+        Address address_test = new Address(23, 45);
+        address_test.setStreet("Inffeldgasse 10");
+        onView(withId(R.id.input_title)).perform(typeText(titleToBeTyped), closeSoftKeyboard());
+        onView(withId(R.id.input_category)).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(1).perform(click());
+        onView(withId(R.id.input_category)).check(matches(not(withText("Sport"))));
+        onView(withId(R.id.input_subcategory)).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(2).perform(click());
+        onView(withId(R.id.input_subcategory)).check(matches(not(withText("Running"))));
+        onView(withId(R.id.btn_calendar)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 4, 3));
+        onView(withId(android.R.id.button1)).perform(click()); //click on dialog positive button
+        mActivityRule.getActivity().entry_address = address_test;
+        onView(withId(R.id.btn_save)).perform(click());
+        Entry entry;
+        Context context = getTargetContext();
+        entry = Helper.getEntryComplete(context, 1);
+        DecimalFormat df2 = new DecimalFormat("###.##");
+        double latitude = Double.valueOf(df2.format(entry.getAddress().getLatitude()));
+        assertTrue(address_test.getLatitude() == latitude);
+    }
+
+    @Test
     public void createEntryWithAddressLatitudeLongitude() {
         TestHelper.grantLocationPermissions();
         Address address_test = new Address(23, 45);
@@ -368,115 +367,6 @@ public class CreateEntryScreenInstrumentedTest {
         double latitude = Double.valueOf(df2.format(entry.getAddress().getLatitude()));
         assertTrue(address_test.getLatitude() == latitude);
     }
-
-    @Test
-    public void pressLocationButton() {
-        TestHelper.grantLocationPermissions();
-        onView(withId(R.id.input_title)).perform(typeText(titleToBeTyped), closeSoftKeyboard());
-        onView(withId(R.id.input_category)).perform(click());
-        onData(allOf(is(instanceOf(String.class)))).atPosition(1).perform(click());
-        onView(withId(R.id.input_category)).check(matches(not(withText("Sport"))));
-        onView(withId(R.id.input_subcategory)).perform(click());
-        onData(allOf(is(instanceOf(String.class)))).atPosition(2).perform(click());
-        onView(withId(R.id.input_subcategory)).check(matches(not(withText("Running"))));
-        onView(withId(R.id.btn_calendar)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 4, 3));
-        onView(withId(android.R.id.button1)).perform(click()); //click on dialog positive button
-        onView(withId(R.id.btn_location)).perform(click());
-        onView(withId(R.id.badge_address)).check(matches(isDisplayed()));
-    }
-
-/*
-    @Test
-    public void shouldOpenAlertDialog(){
-        Address address_test = new Address(23.4500, 45.4500);
-        onView(withId(R.id.btn_location)).perform(click());
-        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void shouldCancelAlertDialog(){
-        Address address_test = new Address(23.4500, 45.4500);
-        onView(withId(R.id.btn_location)).perform(click());
-        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
-        onView(withText("No")).perform(click());
-        onView(withId(R.id.btn_location)).perform(click());
-        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
-    }*/
-
-    @Test
-    public void checkGetLocation() {
-        TestHelper.grantLocationPermissions();
-        GpsLocation gps_location = new GpsLocation(context);
-        //enable GPS to test this
-        Location current_location = new Location("A1");
-        current_location.setLongitude(15);
-        current_location.setLatitude(13);
-        gps_location.current_location = current_location;
-        assertTrue(gps_location.getLocation() != null);
-        //disable GPS to test this
-        /*
-        gps_location.current_location = null;
-        assertTrue(gps_location.getLocation() == null);
-        */
-    }
-
-    /*@Test
-    public void checkGetLocationNoPermission() throws UiObjectNotFoundException {
-        TestHelper.revokePicturePermissions();
-        onView(withId(R.id.btn_location)).perform(click());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            UiObject allowPermissions = mDevice.findObject(new UiSelector().text("Verweigern"));
-            if (allowPermissions.exists()) {
-                allowPermissions.click();
-            }
-        }
-        GpsLocation gps_location = new GpsLocation(context);
-        Location current_location = new Location("A1");
-        current_location.setLongitude(15);
-        current_location.setLatitude(13);
-        gps_location.current_location = current_location;
-        Location location = gps_location.getLocation();
-        assertTrue(location == null);
-    }*/
-
-    @Test
-    public void checkGetLocationWithPermission() {
-        TestHelper.grantLocationPermissions();
-        GpsLocation gps_location = new GpsLocation(context);
-        Location current_location = new Location("A1");
-        current_location.setLongitude(15);
-        current_location.setLatitude(13);
-        gps_location.current_location = current_location;
-
-        Location location = gps_location.getLocation();
-        assertTrue(location.getLatitude() != 0);
-    }
-
-
-    /*@Test
-    public void shouldAcceptAlertDialog() throws Throwable {
-        Address address_test = new Address(23.4500, 45.4500);
-        onView(withId(R.id.btn_location)).perform(click());
-        onView(withText("GPS Not Enabled")).check(matches(isDisplayed()));
-        onView(withText("Yes")).perform(click());
-        mActivityRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mActivityRule.getActivity().onBackPressed();
-            }
-        });
-        intended(hasComponent(CreateEntryScreen.class.getName()));
-    }*/
-
-    /*@Test
-    public void reverseGeocoding() throws IOException {
-        Address address = new Address(13.0, 43.0);
-        ReverseGeocoder reverseGeocoder = new ReverseGeocoder();
-        Address address1;
-        address1 = reverseGeocoder.getAddress(address.getLongitude(), address.getLatitude(), geocoder);
-        assertTrue(address1.getCountry().equals("Italy"));
-    }*/
 
     @Test
     public void addFriends() {
